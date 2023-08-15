edf41a0368f2e36fe51c14eaaab55743af2e8ae3
SYHo <ho2256@aol.com>
2023-08-15 15:15:43 +1000

    Update streamlit_app.py
    add food items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title("Hello Toffee")
 streamlit.header('Sit!  Stop barking.')
 streamlit.text('Good dog!')
+streanlit.text('Do you like 🐔chicken or 🍞bread?')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
