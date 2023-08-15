2b8e57db72af145181b40d221d436ee414c46868
SYHo <ho2256@aol.com>
2023-08-15 15:41:49 +1000

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("Hello Toffee")
 streamlit.header('Sit!  Stop barking.')
 streamlit.text('Good dog!')
-streanlit.text('Do you like 🐔chicken or 🍞bread?')
+streamlit.text('Do you like 🐔chicken or 🍞bread?')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
